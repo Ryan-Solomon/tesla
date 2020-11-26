e9f44be133d0d8ed9191d43566e47f6785b9b8a8
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-11-25 20:24:10 -0600

    add page 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import './App.scss';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Model3 from './pages/model-3/Model3';
 import Nav from './components/nav/Nav';
+import Specs from './components/specs/Specs';
 
 function App() {
   return (
     <Router>
-      <Nav />
+      {/* <Nav /> */}
       <Switch>
         <Route exact path='/'>
           <Model3 />
+        </Route>
+        <Route exact path='/specs'>
+          <Specs />
         </Route>
       </Switch>
     </Router>
--- src/pages/model-3/Model3.tsx
+++ src/pages/model-3/Model3.tsx
@@ -4,12 +4,36 @@ import './Model3.styles.scss';
 
 const Model3 = () => {
   return (
-    <div className='model-3-container'>
-      <header>
-        <h1 className='car-title'>Model 3</h1>
-      </header>
-      <CarInfoComponent />
-    </div>
+    <>
+      <div className='model-3-top-container'>
+        <header>
+          <h1 className='car-title'>Model 3</h1>
+        </header>
+        <CarInfoComponent />
+      </div>
+      <div className='interior-container'>
+        <div className='model-3-interior-container'>
+          <CarInfoComponent />
+        </div>
+        <div className='interior-details'>
+          <div className='title-info'>
+            <h4>Interior</h4>
+            <h1>Built Around The Driver</h1>
+            <h2>+ Learn More</h2>
+          </div>
+          <div className='description'>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Quibusdam molestiae eum aut. Ipsum animi, quasi consequuntur est
+              repellat illum accusantium, maiores quaerat, natus corporis
+              tenetur? Itaque natus saepe, aperiam debitis voluptatibus
+              doloribus vitae pariatur iste nostrum error soluta optio eaque.
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Specs Component */}
+    </>
   );
 };
 
