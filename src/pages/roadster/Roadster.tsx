4575120cfb162d6b4faca32eb6896e189aab1213
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-02 20:48:19 -0600

    add homepage

---
+++ src/pages/roadster/Roadster.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Roadster.styles.scss';
+
+const Roadster = () => {
+  return (
+    <div>
+      <h1>Roadster</h1>
+    </div>
+  );
+};
+
+export default Roadster;
