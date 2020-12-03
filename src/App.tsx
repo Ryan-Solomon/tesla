e0e661d16345f53ff0e119c0e016ccc5f82a189a
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-02 20:50:21 -0600

    add main routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Model3 from './pages/model-3/Model3';
 import Nav from './components/nav/Nav';
 import PaymentPage from './pages/payment/PaymentPage';
+import Home from './pages/home/Home';
+import CyberTruck from './pages/cybertruck/CyberTruck';
+import Roadster from './pages/roadster/Roadster';
+import ModelY from './pages/model-y/ModelY';
+import ModelX from './pages/model-x/ModelX';
+import ModelS from './pages/model-s/ModelS';
 
 function App() {
   return (
@@ -11,7 +17,25 @@ function App() {
       <Nav />
       <Switch>
         <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/model3'>
           <Model3 />
+        </Route>
+        <Route exact path='/modelS'>
+          <ModelS />
+        </Route>
+        <Route exact path='/modelX'>
+          <ModelX />
+        </Route>
+        <Route exact path='/modelY'>
+          <ModelY />
+        </Route>
+        <Route exact path='/roadster'>
+          <Roadster />
+        </Route>
+        <Route exact path='/cybertruck'>
+          <CyberTruck />
         </Route>
         <Route exact path='/payment/:car'>
           <PaymentPage />
