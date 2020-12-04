20e3a05477dfee21eb129e4863b7e23c75a0e524
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-03 19:23:44 -0600

    add fade in

--- src/animations/animations.tsx
+++ src/animations/animations.tsx
@@ -2,3 +2,17 @@ export const titleAnimation = {
   hidden: { opacity: 0 },
   show: { opacity: 1, transition: { duration: 2 } },
 };
+
+export const pageAnimation = {
+  hidden: {
+    opacity: 0,
+    y: 300,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+    },
+  },
+};
--- src/pages/model-3/Model3.tsx
+++ src/pages/model-3/Model3.tsx
@@ -3,46 +3,48 @@ import CarInfoComponent from '../../components/car-info-component/CarInfoCompone
 import './Model3.styles.scss';
 import Specs from './../../components/car-specs/Specs';
 import { motion } from 'framer-motion';
-import { titleAnimation } from './../../animations/animations';
+import { titleAnimation, pageAnimation } from './../../animations/animations';
 
 const Model3 = () => {
   return (
     <>
-      <div className='model-3-top-container'>
-        <header>
-          <motion.h1
-            variants={titleAnimation}
-            initial='hidden'
-            animate='show'
-            className='car-title'
-          >
-            Model 3
-          </motion.h1>
-        </header>
-        <CarInfoComponent />
-      </div>
-      <div className='interior-container'>
-        <div className='model-3-interior-container'>
+      <motion.div variants={pageAnimation} initial='hidden' animate='show'>
+        <div className='model-3-top-container'>
+          <header>
+            <motion.h1
+              variants={titleAnimation}
+              initial='hidden'
+              animate='show'
+              className='car-title'
+            >
+              Model 3
+            </motion.h1>
+          </header>
           <CarInfoComponent />
         </div>
-        <div className='interior-details'>
-          <div className='title-info'>
-            <h4>Interior</h4>
-            <h1>Built Around The Driver</h1>
-            <h2>+ Learn More</h2>
+        <div className='interior-container'>
+          <div className='model-3-interior-container'>
+            <CarInfoComponent />
           </div>
-          <div className='description'>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Quibusdam molestiae eum aut. Ipsum animi, quasi consequuntur est
-              repellat illum accusantium, maiores quaerat, natus corporis
-              tenetur? Itaque natus saepe, aperiam debitis voluptatibus
-              doloribus vitae pariatur iste nostrum error soluta optio eaque.
-            </p>
+          <div className='interior-details'>
+            <div className='title-info'>
+              <h4>Interior</h4>
+              <h1>Built Around The Driver</h1>
+              <h2>+ Learn More</h2>
+            </div>
+            <div className='description'>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Quibusdam molestiae eum aut. Ipsum animi, quasi consequuntur est
+                repellat illum accusantium, maiores quaerat, natus corporis
+                tenetur? Itaque natus saepe, aperiam debitis voluptatibus
+                doloribus vitae pariatur iste nostrum error soluta optio eaque.
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <Specs />
+        <Specs />
+      </motion.div>
     </>
   );
 };
