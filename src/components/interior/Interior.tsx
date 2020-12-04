25abb614fa71e1d5472c34fa2bbb92b2611e228b
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-03 20:18:03 -0600

    add interior component

--- src/components/interior/Interior.tsx
+++ src/components/interior/Interior.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import CarInfoComponent from '../car-info-component/CarInfoComponent';
+import './Interior.styles.scss';
 
 const Interior = () => {
-  return <div>int</div>;
+  return (
+    <div className='interior-container'>
+      <div className='model-3-interior-container'>
+        <CarInfoComponent />
+      </div>
+      <div className='interior-details'>
+        <div className='title-info'>
+          <h4>Interior</h4>
+          <h1>Built Around The Driver</h1>
+          <h2>+ Learn More</h2>
+        </div>
+        <div className='description'>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam
+            molestiae eum aut. Ipsum animi, quasi consequuntur est repellat
+            illum accusantium, maiores quaerat, natus corporis tenetur? Itaque
+            natus saepe, aperiam debitis voluptatibus doloribus vitae pariatur
+            iste nostrum error soluta optio eaque.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Interior;
--- src/pages/model-3/Model3.tsx
+++ src/pages/model-3/Model3.tsx
@@ -5,6 +5,7 @@ import Specs from './../../components/car-specs/Specs';
 import { motion } from 'framer-motion';
 import { titleAnimation, pageAnimation } from './../../animations/animations';
 import LandingHero from '../../components/landing-hero/LandingHero';
+import Interior from '../../components/interior/Interior';
 
 const Model3 = () => {
   return (
@@ -16,27 +17,7 @@ const Model3 = () => {
         exit='exit'
       >
         <LandingHero />
-        <div className='interior-container'>
-          <div className='model-3-interior-container'>
-            <CarInfoComponent />
-          </div>
-          <div className='interior-details'>
-            <div className='title-info'>
-              <h4>Interior</h4>
-              <h1>Built Around The Driver</h1>
-              <h2>+ Learn More</h2>
-            </div>
-            <div className='description'>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Quibusdam molestiae eum aut. Ipsum animi, quasi consequuntur est
-                repellat illum accusantium, maiores quaerat, natus corporis
-                tenetur? Itaque natus saepe, aperiam debitis voluptatibus
-                doloribus vitae pariatur iste nostrum error soluta optio eaque.
-              </p>
-            </div>
-          </div>
-        </div>
+        <Interior />
         <Specs />
       </motion.div>
     </>
