684a5e2e9219046172bc315e56a29251a0ca98a8
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-04 19:39:43 -0600

    add boiler to home and cyber

--- src/pages/cybertruck/CyberTruck.tsx
+++ src/pages/cybertruck/CyberTruck.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import { pageAnimation } from '../../animations/animations';
+import LandingHero from '../../components/landing-hero/LandingHero';
+import Interior from '../../components/interior/Interior';
+import Specs from '../../components/car-specs/Specs';
 
 const CyberTruck = () => {
   return (
-    <div>
-      <h1>Cyber Truck</h1>
-    </div>
+    <motion.div
+      variants={pageAnimation}
+      initial='hidden'
+      animate='show'
+      exit='exit'
+    >
+      <LandingHero backgroundUrl={backgroundImage} title='Cybertruck' />
+      <Interior />
+      <Specs />
+    </motion.div>
   );
 };
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,18 @@
+import { motion } from 'framer-motion';
 import React from 'react';
+import { pageAnimation } from '../../animations/animations';
+import LandingHero from '../../components/landing-hero/LandingHero';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <motion.div
+      variants={pageAnimation}
+      initial='hidden'
+      animate='show'
+      exit='exit'
+    >
+      <LandingHero backgroundUrl={backgroundImage} title='Model X' />
+    </motion.div>
   );
 };
 
