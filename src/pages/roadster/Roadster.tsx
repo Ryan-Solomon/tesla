c4f5540e26db02327e4b3e40e28064e9f2e88fda
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-04 20:36:22 -0600

    add data

--- src/pages/roadster/Roadster.tsx
+++ src/pages/roadster/Roadster.tsx
@@ -1,11 +1,24 @@
+import { motion } from 'framer-motion';
 import React from 'react';
+import { pageAnimation } from '../../animations/animations';
+import Specs from '../../components/car-specs/Specs';
+import Interior from '../../components/interior/Interior';
+import LandingHero from '../../components/landing-hero/LandingHero';
 import './Roadster.styles.scss';
+import backgroundImage from '../../img/roadster.jpg';
 
 const Roadster = () => {
   return (
-    <div>
-      <h1>Roadster</h1>
-    </div>
+    <motion.div
+      variants={pageAnimation}
+      initial='hidden'
+      animate='show'
+      exit='exit'
+    >
+      <LandingHero backgroundUrl={backgroundImage} title='Cybertruck' />
+      <Interior />
+      <Specs />
+    </motion.div>
   );
 };
 
