dba0c30113e5b8a1b2016ff114f8d45a5f9fdfed
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-05 20:57:07 -0600

    add down arrow

--- src/components/car-info-component/CarInfoComponent.tsx
+++ src/components/car-info-component/CarInfoComponent.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './CarInfoComponent.styles.scss';
 import { Redirect, useHistory, useRouteMatch } from 'react-router-dom';
 import PaymentPage from '../../pages/payment/PaymentPage';
+import { AiFillCaretDown } from 'react-icons/ai';
 
 const CarInfoComponent = () => {
   const { path } = useRouteMatch();
@@ -15,27 +16,32 @@ const CarInfoComponent = () => {
   };
 
   return (
-    <section className='car-info'>
-      <div className='car-statistics'>
-        <h3>
-          3.1<span className='seconds'>s</span>
-        </h3>
-        <h6>From 0-60 mph</h6>
-      </div>
-      <div className='car-statistics'>
-        <h3>
-          353<span className='miles'>mi</span>
-        </h3>
-        <h6>Range</h6>
-      </div>
-      <div className='car-statistics'>
-        <h3>AWD</h3>
-        <h6>Dual Motor</h6>
-      </div>
-      <div className='order-button'>
-        <button onClick={directToPayments}>ORDER NOW</button>
-      </div>
-    </section>
+    <>
+      <section className='car-info'>
+        <div className='car-statistics'>
+          <h3>
+            3.1<span className='seconds'>s</span>
+          </h3>
+          <h6>From 0-60 mph</h6>
+        </div>
+        <div className='car-statistics'>
+          <h3>
+            353<span className='miles'>mi</span>
+          </h3>
+          <h6>Range</h6>
+        </div>
+        <div className='car-statistics'>
+          <h3>AWD</h3>
+          <h6>Dual Motor</h6>
+        </div>
+        <div className='order-button'>
+          <button onClick={directToPayments}>ORDER NOW</button>
+        </div>
+        <div className='icon'>
+          <AiFillCaretDown color='grey' size={30} />
+        </div>
+      </section>
+    </>
   );
 };
 
