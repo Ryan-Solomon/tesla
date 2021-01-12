325ce72396386af7e4320ff7bbb8cb4ea9422642
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-12 11:27:24 -0600

    show correct input after user types

--- src/components/account-details-form/AccountDetailsForm.test.tsx
+++ src/components/account-details-form/AccountDetailsForm.test.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import AccountDetailsForm from './AccountDetailsForm';
+import userEvent from '@testing-library/user-event';
 
-it('renders welcome message', () => {
+test('renders welcome message', () => {
   render(<AccountDetailsForm />);
   expect(screen.getByText(/enter account details/i)).toBeInTheDocument();
+});
+
+test('show correct input after user types', () => {
+  render(<AccountDetailsForm />);
+  const firstNameInput = screen.getByLabelText(
+    /first name/i
+  ) as HTMLInputElement;
+  expect(firstNameInput).toBeInTheDocument();
+  userEvent.type(firstNameInput, 'ryan');
+  expect(firstNameInput.value).toEqual('ryan');
 });
